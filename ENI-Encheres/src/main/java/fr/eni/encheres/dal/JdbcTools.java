988491cffbb7b115b20bc7d98e038f2b52455f17
frpoteau <152607603+frpoteau@users.noreply.github.com>
2023-12-05 16:14:45 +0100

    create JdbcTool et Settings

---
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/JdbcTools.java
@@ -0,0 +1,37 @@
+package fr.eni.encheres.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import fr.eni.papeterie.dal.DALException;
+import fr.eni.papeterie.dal.Settings;
+
+public class JdbcTools {
+
+	private static String url;
+	private static String user ;
+	private static String password;
+	
+	static {
+		try {
+			Class.forName(Settings.getProperties("driverdb"));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		url = Settings.getProperties("urldb");
+		user = Settings.getProperties("userdb");
+		password = Settings.getProperties("passworddb");
+	}
+	/**
+	 * Permet la connexion à la BD
+	 * @return con
+	 * @throws DALException
+	 * @throws SQLException 
+	 */
+	public static Connection getConnection() throws SQLException {
+				Connection con = DriverManager.getConnection(url, user, password);
+		return con;
+	}
+}
