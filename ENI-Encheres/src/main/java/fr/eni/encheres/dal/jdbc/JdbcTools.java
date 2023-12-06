10b87801d22e7e3a1c81d1dad64031025fba842f
frpoteau <152607603+frpoteau@users.noreply.github.com>
2023-12-06 15:19:41 +0100

    Update JdbcTools.java
    Ajout closeConnection

--- ENI-Encheres/src/main/java/fr/eni/encheres/dal/jdbc/JdbcTools.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/jdbc/JdbcTools.java
@@ -31,7 +31,23 @@ public class JdbcTools {
 	 * @throws SQLException 
 	 */
 	public static Connection getConnection() throws SQLException {
-				Connection con = DriverManager.getConnection(url, user, password);
-		return con;
+				Connection cnx = DriverManager.getConnection(url, user, password);
+		return cnx;
 	}
+	
+	/**
+	 * Permet la fermeture de la connection
+	 * 
+	 */public static void closeConnection(Connection connection) {
+			try {
+				if(connection!=null && !connection.isClosed()) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				// TODO brancher les messages d'erreur
+	            System.err.println("Erreur lors de la fermeture de la connexion : " + e.getMessage());
+			}
+		}
 }
+	
+	
