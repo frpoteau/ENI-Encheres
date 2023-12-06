466669f316b6f2a6c42ca5439e1df33f13bccabc
KevinV2023 <kevin.viannenc2023@campus-eni.fr>
2023-12-06 11:12:36 +0100

    Update Utilisateur JdbcImpl

--- ENI-Encheres/src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -6,6 +6,16 @@ import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.UtilisateurDAO;
 
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
+	
+	private static final String SQL_INSERT ="INSERT INTO Utilisateur (no_Utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static final String SQL_SELECTEDBY_ID ="SELECT no_Utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, credit, administrateur \"\r\n"
+												+ "	+\"	FROM Utilisateur WHERE noUtilisateur = ?";
+	private static final String SQL_SELECT_ALL
+	
+	
+	
+	
+	
 
 	@Override
 	public void insert(Utilisateur u) {
