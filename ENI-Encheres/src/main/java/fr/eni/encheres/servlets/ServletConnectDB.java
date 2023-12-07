1784a5b02c204c46b0276e1daf6be98540ab9195
YoCode35 <93747400+YoCode35@users.noreply.github.com>
2023-12-07 13:43:09 +0100

    Connexion à la DB externalisée + fichier error (en cas de besoin)

--- ENI-Encheres/src/main/java/fr/eni/encheres/servlets/ServletConnectDB.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/servlets/ServletConnectDB.java
@@ -1,12 +1,14 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,62 +19,59 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet("/ServletConnectDB")
-public class ServletConnectDB extends HttpServlet 
-{
+public class ServletConnectDB extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-    	// Définit le type de contenu de la réponse comme HTML
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        // Récupère les paramètres du formulaire de connexion
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
+        // Charge les propriétés depuis le fichier
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("/fr/eni/encheres/dal/settings.properties")) {
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            out.println("Erreur de chargement du fichier de configuration.");
+            return;
+        }
+
+        String dbDriver = prop.getProperty("driverdb");
+        String dbUrl = prop.getProperty("urldb");
+        String dbUser = prop.getProperty("userdb");
+        String dbPassword = prop.getProperty("passworddb");
+
         try {
-	            // Charge le pilote JDBC pour Microsoft SQL Server
-	            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-	            
-	            // Établit la connexion à la base de données ENCHERES_DB
-	            Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost;databasename=ENCHERES_DB;encrypt=false;trustservercertificate=true", "sa", "Pa$$w0rd");
-	
-	            // Prépare une requête SQL pour vérifier les informations de connexion
-	            PreparedStatement ps = con.prepareStatement("SELECT * FROM UTILISATEURS WHERE email=? AND mot_de_passe=?");
-	            ps.setString(1, email);
-	            ps.setString(2, password);
-	
-	            // Exécute la requête et récupère le résultat
-	            ResultSet rs = ps.executeQuery();
-	
-	            // Vérifie si l'utilisateur existe dans la base de données
-	            if (rs.next()) 
-	            {
-	                // Connexion réussie, stocke la variable de session
-	                HttpSession session = request.getSession();
-	                session.setAttribute("userConnected", true);
-	                session.setAttribute("userEmail", email);
-	
-	                // Redirection vers index.jsp après connexion réussie
-	                RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-	                rd.forward(request, response);
-	            } 
-	            else 
-	            {
-	                // Affiche un message d'erreur si l'email ou le mot de passe est incorrect
-	                out.println("Email ou mot de passe incorrect.");
-	            }   
-	            
-	            // Ferme la connexion après utilisation
-	            con.close();
-        	} 
-        
-	        catch (ClassNotFoundException | SQLException e) 
-	        {
-	            // Affiche les détails de l'erreur en cas de problème de connexion
-	            e.printStackTrace();
-	            out.println("Erreur de connexion à la base de données.");
-	        }
+            Class.forName(dbDriver);
+            Connection con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM UTILISATEURS WHERE email=? AND mot_de_passe=?");
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                // Connexion réussie, stockez la variable de session
+                HttpSession session = request.getSession();
+                session.setAttribute("userConnected", true);
+                session.setAttribute("userEmail", email);
+
+                // Redirection vers index.jsp après connexion réussie
+                RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+                rd.forward(request, response);
+            } else {
+                out.println("Email ou mot de passe incorrect.");
+            }
+
+            con.close(); // Fermez la connexion après utilisation
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            out.println("Erreur de connexion à la base de données.");
+        }
     }
 }
---
+++ ENI-Encheres/src/main/webapp/error.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Erreur</title>
+</head>
+<body>
+    <h1>Une erreur s'est produite</h1>
+    <p>Consultez les journaux pour plus d'informations.</p>
+</body>
+</html>
