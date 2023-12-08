3ab4acd8826003daa927965359ccff2ef0dc3bfc
YoCode35 <93747400+YoCode35@users.noreply.github.com>
2023-12-08 22:36:31 +0100

    Message d'erreur lors d'un pb éventuel de la Connexion à la DB

--- ENI-Encheres/src/main/java/fr/eni/encheres/servlets/ServletConnectDB.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/servlets/ServletConnectDB.java
@@ -1,16 +1,7 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,55 +12,48 @@ import javax.servlet.http.HttpSession;
 import fr.eni.encheres.bll.UtilisateurManager;
 
 @WebServlet("/ServletConnectDB")
-public class ServletConnectDB extends HttpServlet 
-{
+public class ServletConnectDB extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-    {
-        // Définit le type de contenu de la réponse comme HTML
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // Récupère les paramètres du formulaire de connexion
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
+        try {
+            // Vérifie si l'utilisateur existe
+            boolean utilisateurExiste = UtilisateurManager.getInstance().userExists(email, password);
 
-        try 
-        {
-        	// Vérifie si l'utilisateur existe
-        	boolean utilisateurExiste = UtilisateurManager.getInstance().userExists(email, password);
+            // Création de la session
+            HttpSession session = request.getSession(true);
 
-        	if (utilisateurExiste) 
-            {
-                // Connexion réussie, stockez la variable de session
-                HttpSession session = request.getSession();
+            if (utilisateurExiste) {
+                // Connexion réussie, stocke la variable de session
                 session.setAttribute("userConnected", true);
                 session.setAttribute("userEmail", email);
 
-                //Récupère le crédit de l'utilisateur
+                // Récupère le crédit de l'utilisateur
                 int credit = UtilisateurManager.getInstance().getCreditUser(email);
-    	        session.setAttribute("userCredit", credit);
-                
-                
-                // Redirection vers index.jsp après connexion réussie
-                RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-                rd.forward(request, response);
-            }
-            else 
-            {
-                // Affiche un message d'erreur si l'email ou le mot de passe est incorrect
-                out.println("Email ou mot de passe incorrect.");
-            }
+                session.setAttribute("userCredit", credit);
 
-        } 
-        catch (Exception e) 
-        {
-            // Affiche les détails de l'erreur en cas de problème de connexion
-            e.printStackTrace();
-            out.println("Erreur de connexion à la base de données.");
+                // Réinitialise le message d'erreur
+                session.setAttribute("errorMessage", null);
+
+                // Redirection vers index.jsp après connexion réussie
+                response.sendRedirect("index.jsp");
+            } else {
+                // Connexion échouée, stocke le message d'erreur dans la session
+                session.setAttribute("userConnected", false);
+                session.setAttribute("errorMessage", "Email ou mot de passe incorrect.");
+                response.sendRedirect("index.jsp");
+            }
+        } catch (Exception e) {
+            // Connexion échouée, stocke le message d'erreur dans la session
+            HttpSession session = request.getSession();
+            session.setAttribute("userConnected", false);
+            session.setAttribute("errorMessage", "Erreur de connexion à la base de données.");
+            response.sendRedirect("index.jsp");
         }
-        
     }
 }
