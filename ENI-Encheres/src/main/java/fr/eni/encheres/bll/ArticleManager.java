aa59e181d582ed46cddecd3804ede862865d8fbd
frpoteau <152607603+frpoteau@users.noreply.github.com>
2023-12-11 16:43:01 +0100

    Update ArticleManager.java

--- ENI-Encheres/src/main/java/fr/eni/encheres/bll/ArticleManager.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/bll/ArticleManager.java
@@ -17,7 +17,10 @@ public class ArticleManager {
 		articleDAO = DAOFactory.getArticleDAO();
 	}
 	
-	//Obtenir l'instance unique
+	/**
+	 * Obtenir l'instance unique
+	 * @return
+	 */
 	public static ArticleManager getArticleManager() {
 		if(instance == null) {
 			instance = new ArticleManager();
@@ -25,27 +28,43 @@ public class ArticleManager {
 		return instance;
 	}
 	
-	//Ajouter un article
+	/**
+	 * Ajouter un article
+	 * @param a
+	 */
 	public void ajouterArticle(Article a) {
 		articleDAO.insert(a);
 	}
 	
-	//Selection article par ID
+	/**
+	 * Selection article par ID
+	 * @param a
+	 * @return
+	 */
 	public Article selectById (Article a) {
 		return articleDAO.selectById(a);
 	}
 	
-	//Selection de la liste de tous les articles
+	/**
+	 * Selection de la liste de tous les articles
+	 * @return
+	 */
 	public List<Article> selectAll () {
 		return articleDAO.selectAll();
 	}
 	
-	//Modification d'un article
+	/**
+	 * Modification d'un article
+	 * @param a
+	 */
 	public void updateArticle (Article a) {
 		articleDAO.update(a);
 	}
 	
-	//Suppression d'un article
+	/**
+	 * Suppression d'un article
+	 * @param a
+	 */
 	public void deleteArticle (Article a) {
 		articleDAO.delete(a);
 	}
