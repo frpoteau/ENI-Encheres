578b4ba00dfb5c710f65286aad0db2ad9aa05418
frpoteau <152607603+frpoteau@users.noreply.github.com>
2023-12-14 22:30:50 +0100

    Update DAOFactory.java

--- ENI-Encheres/src/main/java/fr/eni/encheres/dal/DAOFactory.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/dal/DAOFactory.java
@@ -10,18 +10,6 @@ import fr.eni.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 public abstract class DAOFactory {
 
-		/*
-		public static EnchereDAO getEnchereDAO() {
-			
-			return new EnchereDAOJdbcImpl();
-		}
-		*/
-	
-		public static RetraitDAO getRetraitDAO() {
-			
-			return new RetraitDAOJdbcImpl();
-		}
-	
 
 	/**
 	 * Permet la création d'une instance ArticleDAOJdbcImpl.
@@ -51,4 +39,21 @@ public abstract class DAOFactory {
 		return new CategorieDAOJdbcImpl();
 	}
 
+	/*
+	public static EnchereDAO getEnchereDAO() {
+		
+		return new EnchereDAOJdbcImpl();
+	}
+	*/
+
+	/**
+	 * Permet la création d'une instance RetraitDAOJdbcImpl.
+	 * @return
+	 */
+	public static RetraitDAO getRetraitDAO() {
+		
+		return new RetraitDAOJdbcImpl();
+	}
+	
+	
 }
