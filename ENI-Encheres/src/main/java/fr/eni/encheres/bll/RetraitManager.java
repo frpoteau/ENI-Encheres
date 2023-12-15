2da99059197c7dc479a67c9707889a069748cf0f
frpoteau <152607603+frpoteau@users.noreply.github.com>
2023-12-15 15:52:38 +0100

    Update RetraitManager.java

--- ENI-Encheres/src/main/java/fr/eni/encheres/bll/RetraitManager.java
+++ ENI-Encheres/src/main/java/fr/eni/encheres/bll/RetraitManager.java
@@ -5,27 +5,23 @@ import fr.eni.encheres.dal.DAOFactory;
 import fr.eni.encheres.dal.RetraitDAO;
 
 public class RetraitManager {
-	
+
 	private static RetraitManager instance;
 	private RetraitDAO retraitDAO;
-	
-	
+
 	private RetraitManager() {
 		retraitDAO = DAOFactory.getRetraitDAO();
 	}
-		
-		public static RetraitManager getRetraitManager() {
-			if(instance == null) {
-				instance = new RetraitManager();
-			}
-			return instance;
+
+	public static RetraitManager getRetraitManager() {
+		if (instance == null) {
+			instance = new RetraitManager();
+		}
+		return instance;
 	}
 
-		public Retrait selectById(int idArticle) {
-			return retraitDAO.selectById(idArticle);
-		}
-	
-	
-	
-	
+	public Retrait selectById(int idArticle) {
+		return retraitDAO.selectById(idArticle);
+	}
+
 }
